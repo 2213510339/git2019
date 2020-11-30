aecf3b1629d39dad5267220c2c58c2bcd547af4e
v_sunhaojie <v_sunhaojie@baidu.com>
2020-11-30 15:52:26 +0800

    fix 配置文件

--- javademo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ javademo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
+import com.example.demo.entity.mapper.UserMapper;
+import configure.*;
 import com.example.demo.properties.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 // TDD 三定律
 
@@ -13,13 +18,40 @@ import org.springframework.boot.test.context.SpringBootTest;
  * 3 只可编写刚好足以通过当前失败测试的生产代码
  */
 // 测试可以拆分为三个环节: 构造测试数据, 操作测试数据, 检验操作是否达到期望结果
+
 @SpringBootTest
 class DemoApplicationTests {
     @Autowired
-    User user;
+    private UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    public void  testMybatis() {
+        User user = userMapper.findUserByNameAndPass("1", "1");
+        for (int i = 60; i < 150; i++) {
+            user.setLoginName("shj" + i);
+            user.setLoginPass("shj" + i);
+            user.setName("name" + i);
+            user.setStatus(i % 4);
+            userMapper.insert(user);
+        }
         System.out.println(user);
+    }
+//    @Autowired
+//    User user;
+
+    @Autowired
+    Drivers drivers;
+
+    @Autowired
+    Cars cars;
+
+//    @Test
+//    void contextLoads() {
+//        System.out.println(user);
+//    }
+    @Test
+    void getUserInfo(){
+        System.out.println(drivers.getCars() == cars);
     }
 
 }
